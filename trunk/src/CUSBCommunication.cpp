f01a50f8aaf2315b64f6e0cfbcf62a701ea02624
ahagmann <ahagmann@dada8251-8aaf-4745-a216-3651466aa593>
2011-09-25 17:55:55 +0000

    removed libusb/version.h

--- trunk/src/CUSBCommunication.cpp
+++ trunk/src/CUSBCommunication.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "CUSBCommunication.h"
-#include <libusb-1.0/version.h>
+//#include <libusb-1.0/version.h>
 #include <iostream>
 #include <sstream>
 #include <stdio.h>
